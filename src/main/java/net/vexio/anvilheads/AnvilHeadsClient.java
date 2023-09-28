43dd33f35aadc9d51451b2e30b64f81346eb49db
bryce <20468242+vexio@users.noreply.github.com>
2023-09-28 01:59:18 -0700

    update example mod

---
+++ src/main/java/net/vexio/anvilheads/AnvilHeadsClient.java
@@ -0,0 +1,10 @@
+package net.vexio.anvilheads;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnvilHeadsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
